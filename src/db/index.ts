d0c18560742275c8fc823faf932127ac76bf25db
Siddhant Krishna <siddhantkrishnaaa@gmail.com>
2026-07-28 02:17:17 +0530

    fix ssl

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import "dotenv/config";
+﻿import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
@@ -16,6 +16,7 @@ export const pool =
   globalForDb.__arenaNextJsPostgresqlPool ??
   new Pool({
     connectionString: databaseUrl,
+    ssl: { rejectUnauthorized: false },
   });
 
 if (process.env.NODE_ENV !== "production") {
